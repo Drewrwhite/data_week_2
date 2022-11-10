f2c5eb5cfd8474c07d51e35675fddc619bfaf125
Drew White <d.white0002@gmail.com>
2022-11-10 10:24:50 -0800

    add shoe class with method and doc strings

--- main.py
+++ main.py
@@ -12,7 +12,37 @@ def fourth_place(list1: list):
     except IndexError:
         print("There is less than 4 items in this list")
 
+#lambda function that takes teo numbers and subtracts number 2 from number 1
 subtracter_lambda = lambda x, y: x - y
 print(subtracter_lambda(10,6))
+
+class Shoe: # create shoe class
+  def __init__(self, US_size: int, color: str, smelly = False: bool):
+    """ Create shoe class that takes size, color and smelly
+
+    Args:
+        US_size (int): shoe size US
+        color (str): color of shoes
+        smelly (bool, optional): are shoes smelly.  Defaults to False.
+    """
+    self.US_size = US_size
+    self.color = color
+    self.smelly = smelly
+  def euro_size(self):
+    """ takes US_size and converts to Euro size
+
+    Returns:
+        String : with Euro sizing
+    """
+    print ()str(self.US_size + 33) + " Euro size"
+
+# two "Shoe" instances to test euro size
+vans = Shoe(11, "black", False)
+nike = Shoe(10, "green", False)
+# print euro size result
+vans.euro_size()
+nike.euro_size()
+
+
 
 
