04692809958b6ce9b0f8fe53bfa9294812500d67
Drew White <d.white0002@gmail.com>
2022-11-10 08:57:57 -0800

    add function for fourth place with doc string

---
+++ main.py
@@ -0,0 +1,13 @@
+def fourth_place(list1):
+    """fourth_place will return 4th item on list and if list has less, filter out IndexError and return string
+
+    Args:
+        list1 (list): list of items 
+
+    Returns:
+        4th item: if list has 4 or more items will return 4th if not will return str
+    """
+    try:
+        return list1[3]
+    except IndexError:
+        print("There is less than 4 items in this list")
